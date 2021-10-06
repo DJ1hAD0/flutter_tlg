989648abf36f9cb8820ac64285b7e63144a7222e
DJ1hAD0 <my.lsd25@gmail.com>
2021-10-06 12:15:42 +0300

    вставил тернаринк в алигнмент сообщений

--- lib/view/MyApp.dart
+++ lib/view/MyApp.dart
@@ -63,7 +63,10 @@ class _MyAppState extends State<MyApp> {
                             itemCount: _msgs.length,
                             itemBuilder: (BuildContext context, int index) {
                               return Row(
-                                  mainAxisAlignment: _msgs[index].keys.toList()[0] == botName ? MainAxisAlignment.end : MainAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      _msgs[index].keys.toList()[0] == botName
+                                          ? MainAxisAlignment.end
+                                          : MainAxisAlignment.start,
                                   children: [
                                     Flexible(
                                       child: Container(
@@ -127,42 +130,39 @@ class _MyAppState extends State<MyApp> {
                 ])),
                 SizedBox(height: 20.0),
                 Form(
-                        key: _formKey,
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                              suffixIcon: IconButton(
-                                  onPressed: () {
-                                    if (_formKey.currentState!.validate()) {
-                                      BotApi().botSendMessage(mybot, _fieldController.text);
-                                      setState(() {
-                                        _msgs.add({botName: _fieldController.text});
-                                        _fieldController.text = '';
-                                      });
-                                    }
-                                  },
-                                  icon: Icon(Icons.send)),
-                              labelText: 'Message',
-                              hintText: 'Enter a text',
+                    key: _formKey,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  BotApi().botSendMessage(
+                                      mybot, _fieldController.text);
+                                  setState(() {
+                                    _msgs.add({botName: _fieldController.text});
+                                    _fieldController.text = '';
+                                  });
+                                }
+                              },
+                              icon: Icon(Icons.send)),
+                          labelText: 'Message',
+                          hintText: 'Enter a text',
 
-                              // icon: Icon(Icons.ac_unit),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.tealAccent, width: 0.0),
-                                borderRadius: BorderRadius.circular(10.0),
-
-
-                              )),
-                          controller: _fieldController,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Type a text';
-                            }
-                          },
-                        )),
-                    /**/
-
+                          // icon: Icon(Icons.ac_unit),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.tealAccent, width: 0.0),
+                            borderRadius: BorderRadius.circular(10.0),
+                          )),
+                      controller: _fieldController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Type a text';
+                        }
+                      },
+                    )),
+                /**/
               ],
             )));
   }
-
-
 }
